89c3cc9326883544df58a86900cdfa45092816ce
Bazrafkan <bazrafkan.mb@gmail.com>
2022-03-20 22:23:56 -0400

    fix: update firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:cycletowork/src/color.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await AppColor.initial();
 
   runApp(const CycleToWorkApp());
